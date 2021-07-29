b25e4a95bfc9d74820ed675b6196168acb764932
Nick Cherepashkin <cherepawka13@mail.ru>
2021-07-29 13:41:38 +0300

    init empty project

---
+++ src/main/kotlin/subtask3/TelephoneFinder.kt
@@ -0,0 +1,9 @@
+package subtask3
+
+class TelephoneFinder {
+
+    // TODO: Complete the following function
+    fun findAllNumbersFromGivenNumber(number: String): Array<String>? {
+        throw NotImplementedError("Not implemented")
+    }
+}
