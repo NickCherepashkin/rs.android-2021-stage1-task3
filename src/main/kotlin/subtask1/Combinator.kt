254aed3854a8eda832d8ff969c30cb9d3af867fa
Nick Cherepashkin <cherepawka13@mail.ru>
2021-07-29 17:19:46 +0300

    add work combinator

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -1,9 +1,27 @@
 package subtask1
 
 class Combinator {
-
-    // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        return calculate(array[0], array[1])
+    }
+
+    private fun factorial (number: Int): Long {
+        var num: Long = number.toLong()
+        if (number > 1)  {
+            num *= factorial(number-1)
+        }
+        return num
+    }
+
+    fun calculate (m: Int, n:Int) : Int? {
+        var x : Int?
+            if (m >= n) {
+            for (count in 1..(n-1)) {
+                x = (factorial(n) / (factorial(n - count) * factorial(count))).toInt()
+                if (x == m) return count
+            }
+        }
+
+        return null
     }
 }
