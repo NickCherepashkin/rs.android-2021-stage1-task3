9fab3cf6ae5ce2b8e6ad883bbcc6b5b0555f93df
Nick Cherepashkin <cherepawka13@mail.ru>
2021-07-30 22:36:17 +0300

    add work phone finder

--- src/main/kotlin/subtask3/TelephoneFinder.kt
+++ src/main/kotlin/subtask3/TelephoneFinder.kt
@@ -4,6 +4,36 @@ class TelephoneFinder {
 
     // TODO: Complete the following function
     fun findAllNumbersFromGivenNumber(number: String): Array<String>? {
-        throw NotImplementedError("Not implemented")
+        return finder(number)
     }
+
+    private fun finder(number: String) :Array<String>? {
+        val resultList: MutableList<String> = mutableListOf()
+
+        val nearbyNumbers = mapOf(
+            '0' to arrayOf("8"),
+            '1' to arrayOf("2","4"),
+            '2' to arrayOf("1","3","5"),
+            '3' to arrayOf("2","6"),
+            '4' to arrayOf("1","5","7"),
+            '5' to arrayOf("2","4","6","8"),
+            '6' to arrayOf("3","5","9"),
+            '7' to arrayOf("4","8"),
+            '8' to arrayOf("5","7","0","9"),
+            '9' to arrayOf("6","8")
+        )
+
+        for(numeric in number.indices) {
+            val listOfNumbers = nearbyNumbers[number[numeric]]
+            if (listOfNumbers != null) {
+                for (digit in listOfNumbers){
+                    resultList.add(number.substring(0,numeric) + digit + number.substring(numeric + 1))
+                }
+            } else {
+                return null
+            }
+        }
+        return resultList.toTypedArray()
+    }
+
 }
